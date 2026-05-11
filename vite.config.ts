30baf819c021614d08e7472cdec20d42a8510b17
EternalHeart <hao131462@qq.com>
2026-05-11 14:19:09 +0800

    chore: 初始化项目基础配置（Vite + React + TS）

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig(({ command }) => {
+  return {
+    base: command === 'build' ? '/gallup-strengths/' : '/',
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      },
+    },
+    server: {
+      hmr: process.env.DISABLE_HMR !== 'true',
+    },
+  };
+});
